be25d632ac7dbe3439a778b32dcc537a3e48e12f
cristina <bolivarmar22@gmail.com>
2025-02-15 19:25:22 +0100

    proyectoc00yshell00

---
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_print_reverse_alphabet.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vbolivar <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 20:06:08 by vbolivar          #+#    #+#             */
+/*   Updated: 2025/02/06 18:57:39 by vbolivar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_print_reverse_alphabet(void)
+{
+	char	letter;
+
+	letter = 'z';
+	while (letter >= 'a')
+	{
+		write (1, &letter, 1);
+		letter--;
+	}
+}
